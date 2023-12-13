e154f54a5510ddadae3ee898f75a8ba4dbabc51d
wikto <wikto@KOMPWIKI>
2023-12-13 13:48:48 +0100

    Add project files.

---
+++ Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj długość prostokąta:");
+            double dlu = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj wysokość prosotkąta:");
+            double wys = double.Parse(Console.ReadLine());
+
+            double diagonal = Math.Sqrt(Math.Pow(dlu, 2) + Math.Pow(wys, 2));
+            Console.WriteLine("Przekątna prostokąta to: " + diagonal);
+        }
+    }
+}
